6e6b8b061126625f97b6a12a4517f8898fe17a8e
Marissa Eppes <marissaleppes@gmail.com>
2019-09-10 10:49:42 -0400

    marissa edits

--- mse.py
+++ mse.py
@@ -7,6 +7,7 @@ Created on Sun Sep  8 19:04:22 2019
 """
 
 from sklearn.metrics import mean_squared_error
+import numpy as np
 
 
 
@@ -18,8 +19,24 @@ def compare_mse(sarima_model, training_set, testing_set):
     train_mse = mean_squared_error(training_set['count'], predict_train[:-1])
     test_mse = mean_squared_error(testing_set['count'], predict_test[:-1])
     
-    print('Training MSE: ', train_mse)
-    print('Testing MSE: ', test_mse)
+    print('Training MSE: ', "{:.2e}".format(train_mse))
+    print('Testing MSE: ', "{:.2e}".format(test_mse))
+    
+    return train_mse, test_mse
+
+def compare_mse_exog(sarima_model, training_df, testing_df):
+    exog_train = np.array([training_df['avg_temp'], training_df['avg_precips']]).T
+    exog_test = np.array([testing_df['avg_temp'], testing_df['avg_precips']]).T
+    predict_train = sarima_model.predict(exog = exog_train)
+    predict_test = sarima_model.predict(start = len(training_df), end = 
+                                        len(training_df) + len(testing_df) - 1,
+                                        exog = exog_test)
+    
+    train_mse = mean_squared_error(training_df['count'], predict_train)
+    test_mse = mean_squared_error(testing_df['count'], predict_test)
+    
+    print('Training MSE: ', "{:.2e}".format(train_mse))
+    print('Testing MSE: ', "{:.2e}".format(test_mse))
     
     return train_mse, test_mse
 
--- prediction_plots.py
+++ prediction_plots.py
@@ -8,9 +8,167 @@ Created on Sun Sep  8 20:25:43 2019
 
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 
 
-def prediction_plot(automodel, training_set, testing_set):
+def prediction_plot(model, training_set, testing_set, p,d,q,P,D,Q,m):
+    
+    if m == 52:
+        freq ='Week'
+        lw = 2
+    elif m == 12:
+        freq = 'Month'
+        lw = 4
+    else:
+        lw = 2
+        freq = '(test)'
+    
+    order = f'({p},{d},{q})'
+    seasonal_order = f'({P},{D},{Q},{m})'
+    
+    predict_train = model.predict()
+    predict_test = model.forecast(len(testing_set))
+    
+    fig = plt.figure(figsize = (20,20))
+    ax1 = plt.subplot(211)
+    ax2 = plt.subplot(212)
+    
+    ax1.plot(training_set.index,training_set['count'], lw = lw, color = 'mediumturquoise')
+    ax1.plot(testing_set.index,testing_set['count'], lw = lw, color = 'blue')
+    ax1.plot(training_set.index,predict_train, lw = lw, color = 'magenta')
+    
+    ax2.plot(training_set.index,training_set['count'], lw = lw, color = 'mediumturquoise')
+    ax2.plot(testing_set.index,testing_set['count'], lw = lw, color = 'blue')
+    ax2.plot(testing_set.index,predict_test, lw = lw, color = 'orange')
+    
+    ax1.set_xlabel('Date', fontsize = 20)
+    ax1.set_ylabel('Count', fontsize = 20)
+    
+    ax2.set_xlabel('Date', fontsize = 20)
+    ax2.set_ylabel('Count', fontsize = 20)
+    
+    ax1.set_title(f'Number of Bike Rentals per {freq} Time Series, SARIMA {order}{seasonal_order}', fontsize = 30)
+    ax2.set_title(f'Number of Bike Rentals per {freq} Time Series, SARIMA {order}{seasonal_order}', fontsize = 30)
+    
+    ax1.legend(['Train','Test', 'Model Prediction on Training Set'],prop={'size': 24})
+    ax2.legend(['Train','Test', 'Model Prediction on Testing Set'],prop={'size': 24})
+    
+def forecast_plot(model, master, n_forecast,p,d,q,P,D,Q,m):
+    
+    if m == 52:
+        freq ='Week'
+        lw = 2
+    elif m == 12:
+        freq = 'Month'
+        lw = 4
+    else:
+        lw = 2
+    
+    order = f'({p},{d},{q})'
+    seasonal_order = f'({P},{D},{Q},{m})'
+    
+    
+    fig = plt.figure(figsize = (20,10))
+    plt.plot(master.index, master['count'], lw = lw, color = 'mediumorchid')
+    
+    
+    forecast = model.predict(len(master),len(master)+ n_forecast-1)
+    
+    forecast_dates = pd.date_range(master.index[-1], freq = 'm', periods=n_forecast + 1).tolist()[1:]
+
+    plt.plot(forecast_dates, forecast, lw = 6, color = 'indigo')
+    
+    plt.xlabel('Date', fontsize = 20)
+    plt.ylabel('Count', fontsize = 20)
+    plt.legend(['Data','Prediction'],prop={'size': 24})
+    plt.title(f'{n_forecast}-{freq} Prediction on Bike Rentals Using SARIMA {order}{seasonal_order}',fontsize = 30)
+    
+    
+    
+    
+    
+def prediction_plot_exog(model, training_df, testing_df, p,d,q,P,D,Q,m):
+    
+    if m == 52:
+        freq ='Week'
+        lw = 2
+    elif m == 12:
+        freq = 'Month'
+        lw = 4
+    else:
+        lw = 2
+        freq = '(test)'
+    
+    order = f'({p},{d},{q})'
+    seasonal_order = f'({P},{D},{Q},{m})'
+    
+    exog_train = np.array([training_df['avg_temp'], training_df['avg_precips']]).T
+    exog_test = np.array([testing_df['avg_temp'], testing_df['avg_precips']]).T
+    
+    predict_train = model.predict(exog = exog_train)
+    predict_test = model.forecast(len(testing_df), exog = exog_test)
+    
+    fig = plt.figure(figsize = (20,20))
+    ax1 = plt.subplot(211)
+    ax2 = plt.subplot(212)
+    
+    ax1.plot(training_df.index,training_df['count'], lw = lw, color = 'mediumturquoise')
+    ax1.plot(testing_df.index,testing_df['count'], lw = lw, color = 'blue')
+    ax1.plot(training_df.index,predict_train, lw = lw, color = 'magenta')
+    
+    ax2.plot(training_df.index,training_df['count'], lw = lw, color = 'mediumturquoise')
+    ax2.plot(testing_df.index,testing_df['count'], lw = lw, color = 'blue')
+    ax2.plot(testing_df.index,predict_test, lw = lw, color = 'orange')
+    
+    ax1.set_xlabel('Date', fontsize = 20)
+    ax1.set_ylabel('Count', fontsize = 20)
+    
+    ax2.set_xlabel('Date', fontsize = 20)
+    ax2.set_ylabel('Count', fontsize = 20)
+    
+    ax1.set_title(f'Number of Bike Rentals per {freq} Time Series, SARIMA {order}{seasonal_order}', fontsize = 30)
+    ax2.set_title(f'Number of Bike Rentals per {freq} Time Series, SARIMA {order}{seasonal_order}', fontsize = 30)
+    
+    ax1.legend(['Train','Test', 'Model Prediction on Training Set'],prop={'size': 24})
+    ax2.legend(['Train','Test', 'Model Prediction on Testing Set'],prop={'size': 24})
+    
+def forecast_plot(model, master, n_forecast,p,d,q,P,D,Q,m):
+    
+    if m == 52:
+        freq ='Week'
+        lw = 2
+    elif m == 12:
+        freq = 'Month'
+        lw = 4
+    else:
+        lw = 2
+    
+    order = f'({p},{d},{q})'
+    seasonal_order = f'({P},{D},{Q},{m})'
+    
+    
+    fig = plt.figure(figsize = (20,10))
+    plt.plot(master.index, master['count'], lw = lw, color = 'mediumorchid')
+    
+    
+    forecast = model.predict(len(master),len(master)+ n_forecast-1)
+    
+    forecast_dates = pd.date_range(master.index[-1], freq = 'm', periods=n_forecast + 1).tolist()[1:]
+
+    plt.plot(forecast_dates, forecast, lw = 6, color = 'indigo')
+    
+    plt.xlabel('Date', fontsize = 20)
+    plt.ylabel('Count', fontsize = 20)
+    plt.legend(['Data','Prediction'],prop={'size': 24})
+    plt.title(f'{n_forecast}-{freq} Prediction on Bike Rentals Using SARIMA {order}{seasonal_order}',fontsize = 30)
+       
+        
+    
+    
+    
+
+
+def auto_prediction_plot(automodel, training_set, testing_set):
     
     if automodel.seasonal_order[3] == 52:
         freq ='Week'
@@ -51,7 +209,7 @@ def prediction_plot(automodel, training_set, testing_set):
     ax1.legend(['Train','Test', 'Model Prediction on Training Set'],prop={'size': 24})
     ax2.legend(['Train','Test', 'Model Prediction on Testing Set'],prop={'size': 24})
     
-def forecast_plot(automodel, master, n_forecast):
+def auto_forecast_plot(automodel, master, n_forecast):
     
     if automodel.seasonal_order[3] == 52:
         freq ='Week'
